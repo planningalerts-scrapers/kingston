cc6f700e3e2fbbd2722e827211bb571c4faeab54
Matthew Landauer <mlandauer@gmail.com>
2019-06-04 16:49:49 +1000

    Make results more similar

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,23 @@
 require "epathway_scraper"
 
+records = []
 EpathwayScraper.scrape(
   "https://online.kingston.vic.gov.au/ePathway/Production",
-  list_type: :all_year, max_pages: 9, year: 2019
+  list_type: :all_year, year: 2019
 ) do |record|
   # Add state on to the address
   record["address"] += ", VIC"
+  record["address"] = record["address"].squeeze(" ")
+  if record["address"] == ", VIC"
+    record["address"] = ", , VIC"
+  end
+  # Format the description like the php scraper
+  record["description"] = record["description"].gsub("\n", " ").squeeze(" ")
+  records << record
+  puts "Scraped record #{record["council_reference"]}"
+end
+
+# Only keep the most recent 270 to match the php scraper
+records.sort{|a,b| b["date_received"] <=> a["date_received"]}[0...270].each do |record|
   EpathwayScraper.save(record)
 end
