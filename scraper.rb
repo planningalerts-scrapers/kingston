48211da4368902622f5c274f1e45f9fcc7044600
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 04:06:11 +1000

    Pick up the current year

--- scraper.rb
+++ scraper.rb
@@ -1,9 +1,14 @@
 require "epathway_scraper"
 
+ENV['MORPH_PERIOD'] ||= DateTime.now.year.to_s
+year = ENV["MORPH_PERIOD"].to_i
+
+puts "Getting data in year '#{year}', changable via MORPH_PERIOD environment"
+
 records = []
 EpathwayScraper.scrape(
   "https://online.kingston.vic.gov.au/ePathway/Production",
-  list_type: :all_year, year: 2019
+  list_type: :all_year, year: year
 ) do |record|
   # Add state on to the address
   record["address"] += ", VIC"
