f4a4983b2e18159dcead17dd561818a2d3a32222
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 04:07:39 +1000

    Scrape all records from the current year

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,6 @@ year = ENV["MORPH_PERIOD"].to_i
 
 puts "Getting data in year '#{year}', changable via MORPH_PERIOD environment"
 
-records = []
 EpathwayScraper.scrape(
   "https://online.kingston.vic.gov.au/ePathway/Production",
   list_type: :all_year, year: year
@@ -18,11 +17,6 @@ EpathwayScraper.scrape(
   end
   # Format the description like the php scraper
   record["description"] = record["description"].gsub("\n", " ").squeeze(" ")
-  records << record
-  puts "Scraped record #{record["council_reference"]}"
-end
 
-# Only keep the most recent 270 to match the php scraper
-records.sort{|a,b| b["date_received"] <=> a["date_received"]}[0...270].each do |record|
   EpathwayScraper.save(record)
 end
