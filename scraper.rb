f5cc4fbad2d4b5e303adbb4f17677133187bb610
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 13:53:17 +1000

    Update to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,3 @@
 require "epathway_scraper"
 
-EpathwayScraper.scrape_and_save(
-  "https://online.kingston.vic.gov.au/ePathway/Production",
-  list_type: :all_this_year, state: "VIC"
-)
+EpathwayScraper.scrape_and_save_authority(:kingston)
